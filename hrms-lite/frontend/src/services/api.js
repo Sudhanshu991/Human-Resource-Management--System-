77f9e64d57ba9860efa2fedc893ca14e40f7c874
Sudhanshu991 <sudhanshumaurya991@gmail.com>
2026-02-06 01:21:01 +0530

    Configure frontend for production backend

--- hrms-lite/frontend/src/services/api.js
+++ hrms-lite/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://human-resource-management-system-1-5tfm.onrender.com",
 });
 
 export default API;
